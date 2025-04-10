32fec2b2a99321a9747354014b068d65960cdc8b
dgd2kor <164500006+dgd2kor@users.noreply.github.com>
2025-04-10 10:48:56 +0530

    Create data_config.py

---
+++ data_analysis/data_config.py
@@ -0,0 +1,7 @@
+class Config:
+    """Configuration file for dataset paths and hyperparameters."""
+    train_annotations = "data/bdd100k_images_100k/bdd100k/labels/bdd100k_labels_images_train.json"
+    val_annotations = "data/bdd100k_images_100k/bdd100k/labels/bdd100k_labels_images_val.json"
+    train_image_path = "data/bdd100k_images_100k/bdd100k/images/100k/train"
+    val_image_path = "data/bdd100k_images_100k/bdd100k/images/100k/val"
+    num_visuals = 5
