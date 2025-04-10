325fef80dd844b3f2898242441cbe8efa875a11d
dgd2kor <164500006+dgd2kor@users.noreply.github.com>
2025-04-10 15:23:44 +0530

    Update dashboard.py

--- data_analysis/dashboard.py
+++ data_analysis/dashboard.py
@@ -6,17 +6,27 @@ from data_config import Config
 st.title("BDD100K Dataset Dashboard")
 
 from analyze_dataset import category_count, distribution_analysis
-
+from parse_json import load_annotations
+from visualize_data import visualize_all_groundtruths, visualize_unique_images, visualize_occluded_truncated
 
 config = Config()
 
-train_annotations = config.train_annotations
-val_annotations = config.val_annotations
+train_annotations_path = config.train_annotations
+val_annotations_path = config.val_annotations
 
-train_count_df = category_count(train_annotations)
-val_count_df = category_count(val_annotations)
+train_count_df = category_count(train_annotations_path)
+val_count_df = category_count(val_annotations_path)
 
 data_distribution_df = distribution_analysis(train_count_df, val_count_df)
+
+# Save visualizations
+train_annotations = load_annotations(config.train_annotations)
+
+visualize_all_groundtruths(config.train_image_path, train_annotations, config.num_visuals)
+
+visualize_unique_images(config.train_image_path, train_annotations, config.num_unique_visuals)
+
+visualize_occluded_truncated(config.train_image_path, train_annotations, config.num_occ_trun_visuals)
 
 
 # Bar Chart: Train vs Val Distribution
