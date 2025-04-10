84000a6a1d617026353a4552021927a026c0e421
dgd2kor <164500006+dgd2kor@users.noreply.github.com>
2025-04-10 15:25:04 +0530

    Update data_config.py

--- data_analysis/data_config.py
+++ data_analysis/data_config.py
@@ -1,8 +1,11 @@
 class Config:
     """Configuration file for dataset paths and hyperparameters."""
-    train_annotations = "data/bdd100k_images_100k/bdd100k/labels/bdd100k_labels_images_train.json"
-    val_annotations = "data/bdd100k_images_100k/bdd100k/labels/bdd100k_labels_images_val.json"
-    train_image_path = "data/bdd100k_images_100k/bdd100k/images/100k/train"
-    val_image_path = "data/bdd100k_images_100k/bdd100k/images/100k/val"
-    visuals_output_path = "./"
+    train_annotations = "/app/data/bdd100k_labels_release/bdd100k/labels/bdd100k_labels_images_train.json"
+    val_annotations = "/app/data/bdd100k_labels_release/bdd100k/labels/bdd100k_labels_images_val.json"
+    train_image_path = "/app/data/bdd100k_images_100k/bdd100k/images/100k/train"
+    val_image_path = "/app/data/bdd100k_images_100k/bdd100k/images/100k/val"
+
     num_visuals = 5
+    num_occ_trun_visuals = 5
+    num_unique_visuals = 5
+    visuals_output_path = "./visuals"
